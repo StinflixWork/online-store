05cf137ffc4cf79c5526fa6430e8c1b0fa852b17
StinflixWork <stinflix.frontsv@gmail.com>
2024-04-04 17:20:26 +0300

    update: fix bugs when hovering over the card

--- src/components/product-item/ProductItem.tsx
+++ src/components/product-item/ProductItem.tsx
@@ -15,29 +15,32 @@ const ProductItem: FC<TypeProps> = ({ product }) => {
 	const price = formatCurrency(product.price)
 
 	return (
-		<div
-			className='group/card w-[270px] flex flex-col gap-y-5 relative p-4
-			border border-solid border-gray-300 hover:shadow-card'
-		>
-			<ProductImages images={product.images} />
-			<div className='flex flex-col gap-y-2.5'>
-				<div className='h-9 overflow-hidden'>
-					<h4 className='leading-5'>
-						<a href='#'>{product.title}</a>
-					</h4>
-				</div>
-				<div className='flex'>
-					<ProductRating />
-				</div>
-				<div className='flex items-center justify-between'>
-					<p className='text-gray-700 font-medium text-xl'>{price}</p>
-					<ProductActions />
-				</div>
-				<div className='flex items-center gap-x-2'>
-					<ProductStatus inStock={product.inStock} />
-				</div>
-				<div>
-					<p className='text-[12px] text-gray-800 leading-5'>{product.summaryDescription}</p>
+		<div className='w-[270px] relative'>
+			<div
+				className='group/card flex flex-col gap-y-5 p-4
+				border border-solid border-gray-300 bg-white
+				hover:shadow-card hover:absolute z-20'
+			>
+				<ProductImages images={product.images} />
+				<div className='flex flex-col gap-y-2.5'>
+					<div className='h-9 overflow-hidden'>
+						<h4 className='leading-5'>
+							<a href='#'>{product.title}</a>
+						</h4>
+					</div>
+					<div className='flex'>
+						<ProductRating />
+					</div>
+					<div className='flex items-center justify-between'>
+						<p className='text-gray-700 font-medium text-xl'>{price}</p>
+						<ProductActions />
+					</div>
+					<div className='flex items-center gap-x-2'>
+						<ProductStatus inStock={product.inStock} />
+					</div>
+					<div className='hidden group-hover/card:block'>
+						<p className='text-[12px] text-gray-800 leading-5'>{product.summaryDescription}</p>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/components/product-item/product-images/ProductImages.tsx
+++ src/components/product-item/product-images/ProductImages.tsx
@@ -9,7 +9,17 @@ export const ProductImages: FC<TypeProps> = ({ images }) => {
 	return (
 		<div className='h-[290px] relative group/card-img'>
 			<a href='#'>
-				<img src={images[0].src} alt={images[0].src} className='w-full h-full object-contain' />
+				<img
+					src={images[0].src}
+					alt={images[0].src}
+					className='w-full h-full object-contain group-hover/card-img:hidden'
+				/>
+				<img
+					src={images[1]?.src || images[0].src}
+					alt={images[1]?.src || images[0].src}
+					className='w-full h-full object-contain absolute inset-0 hidden
+					group-hover/card-img:block'
+				/>
 			</a>
 		</div>
 	)
